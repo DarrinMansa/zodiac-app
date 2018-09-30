b77bcad5fe28c3504ebf419c056d465a578abb38
Darrin Webster <darrinwebster@Darrins-MBP.attlocal.net>
2018-09-30 05:38:19 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+// make array list of of all sun signs and characteristics 
+// (for th sake of time I' going to copy and paste from example)
+
+var signs = [
+	{
+		name: "Aquarius",
+		image: "images/aquarius.png",
+		positivetraits: "Friendly, honest, loyal, inventive, independent, and intellectual.",
+		negativetraits: "Intractible, contrarian, perverse, unpredictable, and detached."
+	},
+	{
+		name: "Pisces",
+		image: "images/pisces.png",
+		positivetraits: "Imaginative, sensitive, compassionate, selfless, and sympathetic.",
+		negativetraits: "Escapist, idealistic, secretive, vague, and weak-willed."
+	},
+	{
+		name: "Aries",
+		image: "images/aries.png",
+		positivetraits: "Adventurous, energetic, courageous, enthusiastic, confident, dynamic, and witty.",
+		negativetraits: "Selfish, quick-tempered, impulsive, and impatient."
+	},
+	{
+		name: "Taurus",
+		image: "images/taurus.png",
+		positivetraits: "Patient, reliable, warmhearted, loving, persistent, and determined.",
+		negativetraits: "Jealous, possessive, resentful, inflexible, and greedy."
+	},
+	{
+		name: "Gemini",
+		image: "images/gemini.png",
+		positivetraits: "Adaptable, versatile, communicative, witty, intellectual, eloquent, and lively.",
+		negativetraits: "Nervous, tense, superficial, inconsistent, and shrewd."
+	},
+	{
+		name: "Cancer",
+		image: "images/cancer.png",
+		positivetraits: "Loving, intuitive, imaginative, cautious, protective, and sympathetic.",
+		negativetraits: "Moody, touchy, and clingy."
+	},
+	{
+		name: "Leo",
+		image: "images/leo.png",
+		positivetraits: "Generous, warmhearted, creative, enthusiastic, open-minded, and faithful.",
+		negativetraits: "Pompous, patronizing, bossy, and intolerant."
+	},
+	{
+		name: "Virgo",
+		image: "images/virgo.png",
+		positivetraits: "Modest, meticulous, reliable, practical, diligent, intelligent, and analytical.",
+		negativetraits: "Fussy, anxious, over-critical, and conservative."
+	},
+	{
+		name: "Libra",
+		image: "images/libra.png",
+		positivetraits: "Diplomatic, romantic, charming, easygoing, sociable, and idealistic.",
+		negativetraits: "Indecisive, changable, gullible, flirtatious, and self-indulgent."
+	},
+	{
+		name: "Scorpio",
+		image: "images/scorpio.png",
+		positivetraits: "Determined, forceful, emotional, intuitive, passionate, exciting, and magnetic.",
+		negativetraits: "Jealous, resentful, compulsive, obsessive, and secretive."
+	},
+	{
+		name: "Sagittarius",
+		image: "images/sagittarius.png",
+		positivetraits: "Jovial, good-humored, honest, intellectual, and philosophical.",
+		negativetraits: "Blindly optimistic, careless, irresponsible, superficial, and tactless."
+	},
+	{
+		name: "Capricorn",
+		image: "images/capricorn.png",
+		positivetraits: "Practical, prudent, ambitious, disciplined, patient, careful, humorous, and reserved.",
+		negativetraits: "Pessimistic, fatalistic, miserly, and grudging."
+	}
+];
